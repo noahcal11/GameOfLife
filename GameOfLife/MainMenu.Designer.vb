79363b195621c24a0cb7c698354d249908b7aa48
Noah <noahcal48@gmail.com>
2018-11-16 20:58:59 -0500

    Initial Commit

--- GameOfLife/MainMenu.Designer.vb
+++ GameOfLife/MainMenu.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMainMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'frmMainMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Name = "frmMainMenu"
+        Me.Text = "Game Of Life"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- GameOfLife/My Project/Application.Designer.vb
+++ GameOfLife/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GameOfLife.Form1
+            Me.MainForm = Global.GameOfLife.frmMainMenu
         End Sub
     End Class
 End Namespace
